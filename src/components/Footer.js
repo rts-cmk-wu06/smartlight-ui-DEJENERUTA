da26265acf070bb224e8e65e7ac617fbaeec3cdf
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-02-04 10:54:59 +0100

    finish hompage component and fix footer isuue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from "react";
+import IconComponent from "./IconComponent";
+import bulb from "../assets/bulb.svg";
+import home from "../assets/home.svg";
+import settings from "../assets/settings.svg";
+
 const Footer = () => {
 	return (
-		<footer className="footer">
-			<p>some icons will come here</p>
+		<footer className="footer flex justify-center gap-40 items-center align-middle">
+			<IconComponent icon={bulb} />
+			<IconComponent icon={home} />
+			<IconComponent icon={settings} />
 		</footer>
 	);
 };
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import Avatar from "@material-ui/core/Avatar";
+import profileImage from "../assets/profile.jpg";
+/* import backArrowImage from "../assets/back-arrow.svg";
+import Avatar from "@material-ui/core/Avatar"; */
 
 const headerVariants = {
 	in: {
@@ -34,19 +38,30 @@ const Room = () => {
 				variants={headerVariants}
 				transition={pageTransition}
 			>
-				<Link className="back text-white" to="/">
-					BedRoom
-				</Link>
+				<div className="room-header flex justify-between">
+					<Link className="back text-white" to="/">
+						<h1 className="room-title p-8">
+							<span className="arrow">⬅️</span> Bed
+							<span className="room block">Room</span>
+						</h1>
+					</Link>
+					<Avatar
+						className="b-1 rounded-full p-8"
+						src={profileImage}
+						style={{ width: "100px", height: "100px" }}
+						alt="profile"
+					/>
+				</div>
 			</motion.div>
 			<motion.div
-				className="white-panel bg-white"
+				className="white-panel bg-white rounded-t-3xl"
 				initial="initial"
 				animate="in"
 				exit="out"
 				variants={whitePanelVariants}
 				transition={pageTransition}
 			>
-				<h1>BedRoom</h1>
+				<h1 className="p-8">BedRoom</h1>
 			</motion.div>
 		</>
 	);
