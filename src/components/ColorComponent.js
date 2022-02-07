7a4f6f678b7c9ee155d29868a2d0ea2cadef2f47
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-02-07 22:55:24 +0100

    fix button component and color component issues

--- src/components/ColorComponent.js
+++ src/components/ColorComponent.js
@@ -1,17 +1,17 @@
 import React from "react";
 const ColorComponent = ({ colors }) => {
 	return (
-		<div className=" w-[50px] h-24 p-8">
-			<h1>colors</h1>
+		<div className="p-8">
+			<h1 className="pb-4">colors</h1>
 			<img src={colors} />
 			<div className=" circle flex gap-4">
-				<div className=" bg-orange-600 p-3"></div>
-				<div className="bg-green-500 p-3"></div>
-				<div className="bg-blue p-3"></div>
-				<div className="bg-purple-900 p-3"></div>
-				<div className="bg-pink-400 p-3"></div>
-				<div className="bg-yellow-500 p-3"></div>
-				<div className="bg-white p-3">+</div>
+				<button className=" bg-orang p-3"></button>
+				<button className="bg-green p-3"></button>
+				<button className="bg-lightBlue p-3"></button>
+				<button className="bg-purple p-3"></button>
+				<button className="bg-pink p-3"></button>
+				<button className="bg-darkYellow p-3"></button>
+				<button className="bg-white p-3">+</button>
 			</div>
 		</div>
 	);
--- src/components/Room.js
+++ src/components/Room.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import LightComponent from "./LightComponent";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import Avatar from "@material-ui/core/Avatar";
 import arrowBack from "../assets/back-arrow.svg";
-import lightBulbImage from "../assets/light-bulb.jpg";
 import surfaceImage from "../assets/surface.svg";
 import furnitureImage from "../assets/furniture-household.svg";
 import bedLilleImage from "../assets/bed-lille.svg";
@@ -15,6 +13,7 @@ import Slider from "react-input-slider";
 import ColorComponent from "./ColorComponent";
 import ScenesComponent from "./ScenesComponent";
 import scenesImage from "../assets/scenes-light.svg";
+import Lamp from "./Lamp";
 
 const headerVariants = {
 	in: {
@@ -70,14 +69,9 @@ const Room = () => {
 						</h1>
 						<p className="text-goldenYello p-8 pt-0 pb-2">4 Lights</p>
 					</Link>
-					<Avatar
-						className="b-1 rounded-full p-8"
-						src={lightBulbImage}
-						style={{ width: "150px", height: "150px" }}
-						alt="profile"
-					/>
+					<Lamp color="#FFD139" />
 				</div>
-				<div className="flex justify-between items-center p-8 bottom-4">
+				<div className="flex justify-between items-center p-8 pb-2">
 					<LightComponent image={surfaceImage} txt="Main Lights" />
 					<LightComponent image={furnitureImage} txt="Desk lights" />
 					<LightComponent image={bedLilleImage} txt="Bed" />
@@ -98,8 +92,8 @@ const Room = () => {
 					alt=""
 				/>
 				<div className="w-full">
-					<h1 className="p-8">Intensity</h1>
-					<div className="lights flex justify-between items-end w-full p-8">
+					<h1 className="p-8 pb-2">Intensity</h1>
+					<div className="lights flex justify-between items-end w-full p-8 pb-0 pt-0">
 						<img
 							src={solutionRight}
 							style={{ width: "50px", height: "50px" }}
@@ -138,13 +132,18 @@ const Room = () => {
 							</div>
 						</div>
 						<img
+							className="solution-left"
 							src={solutionLeft}
-							style={{ width: "50px", height: "50px" }}
+							style={{
+								width: "50px",
+								height: "50px",
+							}}
 							alt=""
 						/>
 					</div>
 					<ColorComponent />
-					<div className="flex grid grid-cols-2 gap-4 p-8">
+					<h1 className="p-8 pb-4">Scenes</h1>
+					<div className=" grid grid-cols-2 gap-4 p-8 pt-0">
 						<ScenesComponent image={scenesImage} txt="BirthDay" />
 						<ScenesComponent image={scenesImage} txt="Party" />
 						<ScenesComponent image={scenesImage} txt="Relax" />
