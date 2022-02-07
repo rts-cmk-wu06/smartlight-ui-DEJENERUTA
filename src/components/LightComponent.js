ff8c02ddfa6e2b7a498998f897abc4ce2499123b
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-02-07 13:30:09 +0100

    add color component

--- src/components/LightComponent.js
+++ src/components/LightComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 const LightComponent = ({ image, txt }) => {
 	return (
 		<div className="light-container flex items-center justify-center  bg-white text-lightblue pl-8 pr-8 rounded-xl gap-2">
-			<img src={image} />
+			<img src={image} alt="light" />
 			<p className="text-sm ">{txt}</p>
 		</div>
 	);
